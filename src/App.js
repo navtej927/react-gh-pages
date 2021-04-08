39d563a021173858664f9c9d9d7f97643550e27e
navtej927 <navtej927@gmail.com>
2021-04-08 15:08:35 -0700

    saved project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
+import About from './about';
+import Home from './home';
+import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Home Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default Home;
