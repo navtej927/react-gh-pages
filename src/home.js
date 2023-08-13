61fb27d3d5cd8394be2b797fe7b4d0efb20ba6e0
navtsingh <navtsingh@ladmins-MacBook-Pro-3.local>
2023-08-12 20:35:18 -0700

    updated home page

--- src/home.js
+++ src/home.js
@@ -6,10 +6,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h2>Home Page v5</h2>
-                <main>
-                    <p>This section contains information about...</p>
-                </main>
+                <h2>Home Page V1</h2>
             </div>
         )
     }
